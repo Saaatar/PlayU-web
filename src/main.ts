10969fa31209214bad881ea84a4bab78d8b2a7f2
JazDzib <a22216871@alumnos.uady.mx>
2026-04-13 19:56:07 -0600

    fit: rename function and add it to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import kaplay from "kaplay";
 import { registerCatchFaceGame } from "./games/catch-face";
 import { registerMemocatGame } from "./games/memocat";
+import { registerTuttiFruttiMix } from "./games/tuttiFruttiMix";
 
 const k = kaplay({
   global: false,
@@ -12,6 +13,7 @@ k.loadRoot("./");
 //games
 registerCatchFaceGame(k);
 registerMemocatGame(k);
+registerTuttiFruttiMix(k);
 
 //must be go to menu scene by default
 k.go("catch-face");
